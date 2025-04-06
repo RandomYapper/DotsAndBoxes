0bdd569cbf38964c30e8f3f58b913b366323ca27
shivam <shivamsoni4335@gmail.com>
2025-04-07 01:15:26 +0530

    boilerplate of frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div>hello</div>;
+  return (
+    <div className="p-4">
+      <h1 className="text-4xl text-blue-500">Hello, Tailwind!</h1>
+      <p className="mt-2 text-gray-700">
+        If you see blue heading text, Tailwind is working!
+      </p>
+    </div>
+  );
 }
 
 export default App;
